1deb8ba20c0b3315023b53fc494e0b89a2d2719b
Marvin Knapp <marvin.knapp1@web.de>
2024-11-20 21:27:55 +0100

    feat: add messenger logic

---
+++ src/Controller/FirstMessageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Message\FirstMessage;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FirstMessageController extends AbstractController
+{
+  #[Route('/message', name: 'blaaaaaa')]
+  public function index(MessageBusInterface $messageBus): JsonResponse
+  {
+    $message = new FirstMessage('test');
+
+    $envelope = new Envelope(
+      $message,
+      [new AmqpStamp('user.create')]
+    );
+
+    $messageBus->dispatch($envelope);
+
+    return $this->json([
+      'message' => 'dispatched',
+    ]);
+  }
+}
